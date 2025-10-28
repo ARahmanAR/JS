ef4e2ab08ba27c3efc47f34aeb67bb2d565a358a
Abdur Rahman <abdurrahman262023@gmail.com>
2025-10-28 15:08:34 +0000

    init

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 2450660
+let accountEmail = "user@example.com"
+var accountPassword = "securepassword123" // Not recommended to use 'var' for variable declaration
+accountCity = "New York"
+let accountState;
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountState])
